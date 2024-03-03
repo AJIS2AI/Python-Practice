615a6c69e237d7d7e2b5de435d954c8b93422fdd
Songhui An <soert654@naver.com>
2024-03-03 09:30:03 +0900

    Create Collatz_Conjecture.py

---
+++ Collatz_Conjecture.py
@@ -0,0 +1,10 @@
+def solution(n):
+    result = [n]
+    while n != 1:
+        if n % 2 == 0:
+            n /= 2
+        else :
+            n = n * 3 + 1
+        result.append(n)
+    
+    return result
